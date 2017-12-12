de87247e693f9db562693a30ad8a0ce8c553b178
Matt Ha <matthewha1993@gmail.com>
2017-12-11 21:52:24 -0800

    Add logic for make_move and next_turn

--- W2D2/mancala/lib/board.rb
+++ W2D2/mancala/lib/board.rb
@@ -27,22 +27,34 @@ class Board
   end
 
   def make_move(start_pos, current_player_name)
-    idx = start_pos
-    num_stones = @cups[idx].length
-    @cups[idx] = []
-    i = 0
+    stones = @cups[start_pos]
+    @cups[start_pos] = []
 
-    # TODO: Insert logic and call next_turn
+    cup_idx = start_pos
+    until stones.empty?
+      cup_idx += 1
+      cup_idx = 0 if cup_idx > 13
+      if cup_idx == 6
+        @cups[6] << stones.pop if current_player_name == @name1
+      elsif cup_idx == 13
+        @cups[13] << stones.pop if current_player_name == @name2
+      else
+        @cups[cup_idx] << stones.pop
+      end
+    end
+    render
+    next_turn(cup_idx)
   end
 
   def next_turn(ending_cup_idx)
     # helper method to determine what #make_move returns
     if ending_cup_idx == 6 || ending_cup_idx == 13
-      return false
-    elsif !@cups[ending_cup_idx].empty?
-      return false
+      :prompt
+    elsif @cups[ending_cup_idx].count == 1
+      # Empty cup since the stone in the cup is the one we just placed.
+      :switch
     else
-      return true
+      ending_cup_idx
     end
   end
 
