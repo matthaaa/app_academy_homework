ce58a7ed548f2d7922acb0c6b84c8d37aa599153
Matt Ha <matthewha1993@gmail.com>
2018-01-10 21:33:58 -0800

    Create part 1 of JS hmk

--- W5D4/part1.js
+++ W5D4/part1.js
@@ -9,13 +9,41 @@ function isSubstring(searchString, subString) {
 }
 
 function fizzBuzz(arr) {
+  const result = [];
 
+  array.forEach(element => {
+    if ((element % 3 === 0) ^ (element % 5 === 0)) {
+      result.push(el);
+    }
+  });
+
+  return result;
 }
 
 function isPrime(num) {
+  if (n < 2) { return false }
 
+  for (let i = 2; i < n; i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 function sumOfNPrimes(n) {
+  let sum = 0;
+  let countPrimes = 0;
+  let i = 2;
 
+  while (countPrimes < n) {
+    if (isPrime(i)) {
+      sum += i;
+      countPrimes++;
+    }
+    i++;
+  }
+
+  return sum;
 }
