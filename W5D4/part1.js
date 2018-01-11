99c2c6ba8d5cbb32db7b88594753212b675ce6c8
Matt Ha <matthewha1993@gmail.com>
2018-01-10 19:46:08 -0800

    Set up functions for part1 js

--- W5D4/part1.js
+++ W5D4/part1.js
@@ -1,5 +1,21 @@
-function madLib (word1, word2, word3) {
+function madLib(word1, word2, word3) {
   return (
     `We shall ${word1.toUpperCase()} the ${word2.toUpperCase()} ${word3.toUpperCase()}.`
   )
+}
+
+function isSubstring(searchString, subString) {
+  return searchString.includes(subString);
+}
+
+function fizzBuzz(arr) {
+
+}
+
+function isPrime(num) {
+
+}
+
+function sumOfNPrimes(n) {
+
 }
