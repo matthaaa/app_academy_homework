20e9bd68cb585ad3bb81050658628078d664f5ba
Matt Ha <matthewha1993@gmail.com>
2018-01-11 22:34:15 -0800

    Fix tea and biscuit method

--- W5D5/javascript.js
+++ W5D5/javascript.js
@@ -16,20 +16,18 @@ const reader = readline.createInterface({
 });
 
 function teaAndBiscuits () {
-  let first, second;
+  reader.question('Would you like some tea?', function (res) {
 
-  reader.question('Would you like some tea?', (res) => {
-    first = res;
     console.log(`You replied ${res}.`);
-  });
+    
+    reader.question('Would you like some biscuits?', function (res2) {
+      console.log(`You replied ${res2}.`);
 
-  reader.question('Would you like some biscuits?', (res) => {
-    second = res;
-    console.log(`You replied ${res}.`);
-  });
+      const first = (res === 'yes') ? 'do' : 'don\'t';
+      const second = (res2 === 'yes') ? 'do' : 'don\'t';
 
-  const firstRes = (first === 'yes') ? 'do' : 'don\'t';
-  const secondRes = (second === 'yes') ? 'do' : 'don\'t';
-  console.log(`So you ${firstRes} want tea and you ${secondRes} want biscuits.`);
-  reader.close();
+      console.log(`So you ${first} want tea and you ${second} want biscuits.`);
+      reader.close();
+    });
+  });
 }
