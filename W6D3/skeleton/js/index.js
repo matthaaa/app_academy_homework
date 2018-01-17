88d35139a9c1e0495bf4a8a36e308de6018af322
Matt Ha <matthewha1993@gmail.com>
2018-01-16 20:56:23 -0800

    Add ajax request to file

--- W6D3/skeleton/js/index.js
+++ W6D3/skeleton/js/index.js
@@ -1,5 +1,16 @@
 console.log("Hello from the JavaScript console!");
 
 // Your AJAX request here
-
+$.ajax({
+  type: 'GET',
+  url: "http://api.openweathermap.org/data/2.5/weather?q=NY,NY&appid=03739854bd818e374da405d52ab7f9b9",
+  success(data) {
+    console.log("Here is the weather today!")
+    console.log(data);
+  },
+  error() {
+    console.error("Error; could not retrieve weather from the interwebs.");
+  },
+});
 // Add another console log here, outside your AJAX request
+console.log("End log");
