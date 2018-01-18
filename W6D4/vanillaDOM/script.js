6cb6c41da54dece51bdab368049a6ffdfadab552
Matt Ha <matthewha1993@gmail.com>
2018-01-18 01:30:41 -0800

    Add toggle and SF places list items

--- W6D4/vanillaDOM/script.js
+++ W6D4/vanillaDOM/script.js
@@ -20,6 +20,24 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // --- your code here!
 
+  const handleFavoriteSubmit = (e) => {
+    e.preventDefault();
+
+    const favoriteInput = document.querySelector(".favorite-input");
+    const favorite = favoriteInput.value;
+    favoriteInput.value = "";
+
+    const newListLi = document.createElement("li");
+    newListLi.textContent = favorite;
+
+    const favoritesList = document.getElementById("sf-places");
+    favoritesList.appendChild(newListLi);
+  };
+
+  const listSubmitButton = document.querySelector(".favorite-submit");
+  listSubmitButton.addEventListener("click", handleFavoriteSubmit);
+
+
 
 
   // adding new photos
